61900c59e3c081782849aa99cdf15e7d56aad664
Chaan <rnchagwe27@gmail.com>
2024-06-01 05:52:41 +0800

    conditional statement

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const prompt = require("prompt-sync")()
 
-prompt("Enter something: ")
+console.log("Welcome to the Computer Hardware Quiz")
+
+const quest1 = prompt("What is the brain of the computer: ")
+const correct1 = "CPU"
+
+if (quest1 === correct1.toLocaleLowerCase()) {
+    console.log("You got it correct!")
+} else {
+    console.log("Sorry. Wrong answer")
+}
