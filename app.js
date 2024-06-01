e2cb324518717c3870c753ac69aef1c3420ac8e4
Chaan <rnchagwe27@gmail.com>
2024-06-01 22:24:23 +0800

    added score var

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const prompt = require("prompt-sync")()
 const quest1 = prompt("What is the brain of the computer: ")
 const correct1 = "CPU"
 
+let score = 0;
+
 console.log("Welcome to the Computer Hardware Quiz")
 
 if (quest1.toUpperCase() === correct1) {
@@ -9,3 +11,4 @@ if (quest1.toUpperCase() === correct1) {
 } else {
     console.log("Sorry. Wrong answer")
 }
+
