cd73846c8718495be0669d44def6570a030cba68
Chaan <rnchagwe27@gmail.com>
2024-06-01 22:23:34 +0800

    aligned

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const prompt = require("prompt-sync")()
-
-console.log("Welcome to the Computer Hardware Quiz")
-
 const quest1 = prompt("What is the brain of the computer: ")
 const correct1 = "CPU"
 
-if (quest1 === correct1.toLocaleLowerCase()) {
+console.log("Welcome to the Computer Hardware Quiz")
+
+if (quest1.toUpperCase() === correct1) {
     console.log("You got it correct!")
 } else {
     console.log("Sorry. Wrong answer")
